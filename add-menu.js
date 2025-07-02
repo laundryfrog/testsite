955e65ee1b1268ffcf2bdedbd51d19b7b585b6b8
Abenthy <132914640+laundryfrog@users.noreply.github.com>
2025-07-02 15:02:50 +0200

    Update add-menu.js

--- add-menu.js
+++ add-menu.js
@@ -1,19 +1,4 @@
 // Check if file is completely ready
   $(document).ready(function () {
-
-      // Adding click event on id div-1
-      // if it clicked then anonymous
-      // function will be called
-      $('#div-1').click(function () {
-
-          // Load the external html
-          // here this refers to 
-          // current selector
-          $(this).load('div-1.html');
-      });
-
-      // Same as above
-      $('#div-2').click(function () {
-          $(this).load('div-2.html');
-      });
+    $('#div-1').load('div-1.html');
   });
